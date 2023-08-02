99dafc0a519851f66e27775c0e14481e2e6cebfa
Radamés Rodrigues <radames.idiomas@gmail.com>
2023-08-01 23:26:21 -0300

    "Modifying app.blade.php"

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
-        crossorigin="anonymous" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous" />
+    <link href="{{ asset('/css/app.css') }}" rel="stylesheet" />
     <title>@yield('title', 'Online Store')</title>
 </head>
 
@@ -35,6 +35,23 @@
     <div class="container my-4">
         @yield('content')
     </div>
+
+    <!-- footer -->
+    <div class="copyright py-4 text-center text-white">
+        <div class="container">
+            <small> Copyright - 
+                <a 
+                    class="text-reset fw-bold text-decoration-none" 
+                    target="_blank"
+                    href="https://github.com/Sapiosonic"
+                >
+                    Radamés Rodrigues
+                </a> - <b>Paola Vallejo</b>
+            </small>
+        </div>
+    </div>
+    <!-- footer -->
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
     </script>
 </body>
