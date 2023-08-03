52ceaf6f3d91b86b8385e0f30847cdb336c8da92
Radamés Rodrigues <radames.idiomas@gmail.com>
2023-08-02 21:30:10 -0300

    "Fixing error in controller' namespace"

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,8 +1,6 @@
 <?php
-
-  namespace App \ Http \ Controllers;
-  use Illuminate \ Http \ Request;
-
+  namespace App\Http\Controllers;
+  use Illuminate\Http\Request;
   class HomeController extends Controller
   {
 
@@ -13,18 +11,17 @@
       return view('home.index')->with("viewData", $viewData);
     }
 
-    public function about()
-    {
-      $data1 = "About us - Online Store";
-      $data2 = "About us";
-      $description = "This is an about page...";
-      $author = "Developed by: Radamés Rodrigues"
-
-      return view('home.about')
-      ->with("title", $data1)
-      ->with("subtitle", $data2)
-      ->with("description", $description)
-      ->with("author", $author);
-    }
+  public function about()
+  {
+    $data1 = "About us - Online Store";
+    $data2 = "About us";
+    $description = "This is an about page ...";
+    $author = "Developed by: Radamés Rodrigues";
+    return view('home.about')->with("title", $data1)
+    ->with("subtitle", $data2)
+    ->with("description", $description)
+    ->with("author", $author);
   }
+}
+
 ?>
