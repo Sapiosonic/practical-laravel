6c53d479c5cc86188ca6e1770a19cb90a736a83f
Radamés Rodrigues <radames.idiomas@gmail.com>
2023-08-02 21:31:00 -0300

    "Correct indentation"

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,17 +11,16 @@
       return view('home.index')->with("viewData", $viewData);
     }
 
-  public function about()
-  {
-    $data1 = "About us - Online Store";
-    $data2 = "About us";
-    $description = "This is an about page ...";
-    $author = "Developed by: Radamés Rodrigues";
-    return view('home.about')->with("title", $data1)
-    ->with("subtitle", $data2)
-    ->with("description", $description)
-    ->with("author", $author);
+    public function about()
+    {
+      $data1 = "About us - Online Store";
+      $data2 = "About us";
+      $description = "This is an about page ...";
+      $author = "Developed by: Radamés Rodrigues";
+      return view('home.about')->with("title", $data1)
+      ->with("subtitle", $data2)
+      ->with("description", $description)
+      ->with("author", $author);
+    }
   }
-}
-
 ?>
